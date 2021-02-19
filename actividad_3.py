c5823543d86798abe1f8c90a5971f45b5da1cf75
Mario Ortega <marioal9806@hotmail.com>
2021-02-19 17:22:08 -0600

    Fix Indentation

--- actividad_3.py
+++ actividad_3.py
@@ -17,18 +17,17 @@ pinMode(button,"INPUT")
 
 while True:
 	try:
-
-button_status= digitalRead(button)
+        button_status= digitalRead(button)
 		if button_status:
 			setRGB(0,255,0)
 			curr_temp = temp(sensor, '1.1')
-setText(f"Temp = {curr_temp} C")	
+            setText(f"Temp = {curr_temp} C")	
 		else:		
-setRGB(255,0,0)
-setText("Presiona el boton")	
+            setRGB(255,0,0)
+            setText("Presiona el boton")	
 					
 	
-except KeyboardInterrupt:	
-break
+    except KeyboardInterrupt:	
+        break
 	except (IOError,TypeError) as e:
 		print("Error")
